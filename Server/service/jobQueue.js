299ae95a0d9841cf3254aa79db21fa22e7036171
Alex Holliday <ajhollid@gmail.com>
2025-01-06 14:30:48 -0800

    Add more logging to jobqueue

--- Server/service/jobQueue.js
+++ Server/service/jobQueue.js
@@ -217,12 +217,37 @@ class NewJobQueue {
 			connection: this.connection,
 			concurrency: 5,
 		});
+
+		worker.on("active", (job) => {
+			this.logger.info({
+				message: `Worker started processing job: ${job.id}`,
+				service: SERVICE_NAME,
+				method: "createWorker",
+			});
+		});
+
+		worker.on("completed", (job) => {
+			this.logger.info({
+				message: `Worker completed job: ${job.id}`,
+				service: SERVICE_NAME,
+				method: "createWorker",
+			});
+		});
+
 		worker.on("failed", (job, err) => {
 			this.logger.error({
 				message: `Worker failed job: ${job.id}`,
 				service: SERVICE_NAME,
 				method: "createWorker",
 				stack: err.stack,
+			});
+		});
+
+		worker.on("stalled", (jobId) => {
+			this.logger.warn({
+				message: `Worker stalled job: ${jobId}`,
+				service: SERVICE_NAME,
+				method: "createWorker",
 			});
 		});
 		return worker;
@@ -353,8 +378,9 @@ class NewJobQueue {
 					const jobs = await queue.getJobs();
 					const ret = await Promise.all(
 						jobs.map(async (job) => {
+							console.log(job);
 							const state = await job.getState();
-							return { url: job.data.url, state };
+							return { url: job.data.url, state, progress: job.progress };
 						})
 					);
 					stats[name] = { jobs: ret, workers: this.workers[name].length };
