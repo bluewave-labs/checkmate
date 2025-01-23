883c47bbe4ac973f547ca3501efcc123278004ae
yoyounik <sinhanikhil909@gmail.com>
2025-01-23 18:41:25 +0530

    Added new pattern

--- Server/validation/joi.js
+++ Server/validation/joi.js
@@ -40,14 +40,14 @@ const registrationBodyValidation = joi.object({
 	firstName: joi
 		.string()
 		.required()
-		.pattern(/^[\p{L}]+(?:[\p{L}\s]+)*$/u)
+		.pattern(/^[\p{L}]+(?:[\p{L}\s'\-]+)*$/u)
 		.messages({
 			"string.pattern.base": "First name must contain only letters and spaces",
 		}),
 	lastName: joi
 		.string()
 		.required()
-		.pattern(/^[\p{L}]+(?:[\p{L}\s]+)*$/u)
+		.pattern(/^[\p{L}]+(?:[\p{L}\s'\-]+)*$/u)
 		.messages({
 			"string.pattern.base": "Last name must contain only letters and spaces",
 		}),
