22cae67ffef202892d0e4cb235a99232645dc8e0
YinDongfang <1136005348@qq.com>
2025-01-27 11:37:44 +0800

    Fix redundant instantiation of NetworkService

--- Client/src/Utils/NetworkServiceProvider.jsx
+++ Client/src/Utils/NetworkServiceProvider.jsx
@@ -1,14 +1,16 @@
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { setNetworkService } from "./NetworkService";
-import NetworkService from "./NetworkService";
+import NetworkService, { networkService } from "./NetworkService";
 import { store } from "../store";
 
 const NetworkServiceProvider = ({ children }) => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
-	const networkService = new NetworkService(store, dispatch, navigate);
-	setNetworkService(networkService);
+	if (!networkService) {
+		const networkService = new NetworkService(store, dispatch, navigate);
+		setNetworkService(networkService);
+	}
 	return children;
 };
 
