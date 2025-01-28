7e85bc3690f341349138f7fc6a1f22156e220c5c
cihatata <omercihatataa@gmail.com>
2025-01-29 01:57:49 +0300

    fix: create stringService

--- Server/middleware/languageMiddleware.js
+++ Server/middleware/languageMiddleware.js
@@ -1,7 +1,9 @@
-const languageMiddleware = (req, res, next) => {
+const languageMiddleware = (stringService, translationService) => (req, res, next) => {
   const acceptLanguage = req.headers['accept-language'] || 'en';
+  const language = acceptLanguage.split(',')[0].slice(0, 2).toLowerCase();
 
-  req.language = acceptLanguage.split(',')[0].slice(0, 2).toLowerCase();
+  translationService.setLanguage(language);
+  stringService.setLanguage(language);
 
   next();
 };
