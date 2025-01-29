6788b5672e3cd05924503711fb3af408b429aae1
Alex Holliday <ajhollid@gmail.com>
2025-01-29 11:39:17 -0800

    add ref to stop initial search

--- Client/src/Pages/Uptime/Monitors/Components/SearchComponent/index.jsx
+++ Client/src/Pages/Uptime/Monitors/Components/SearchComponent/index.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import Search from "../../../../../Components/Inputs/Search";
 import { Box } from "@mui/material";
 import useDebounce from "../../Hooks/useDebounce";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
-const SearchComponent = ({ monitors, onSearchChange, setIsSearching }) => {
+const SearchComponent = ({ monitors = [], onSearchChange, setIsSearching }) => {
+	const isFirstRender = useRef(true);
 	const [localSearch, setLocalSearch] = useState("");
 	const debouncedSearch = useDebounce(localSearch, 500);
 	useEffect(() => {
+		if (isFirstRender.current === true) {
+			isFirstRender.current = false;
+			return;
+		}
 		onSearchChange(debouncedSearch);
 		setIsSearching(false);
 	}, [debouncedSearch, onSearchChange, setIsSearching]);
