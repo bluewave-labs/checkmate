72460b15d10c9537a12272cd0eb20622ea97e4b5
Alex Holliday <ajhollid@gmail.com>
2025-02-05 12:53:47 -0800

    add DU routes

--- Server/routes/distributedUptimeRoute.js
+++ Server/routes/distributedUptimeRoute.js
@@ -8,6 +8,14 @@ class DistributedUptimeRoutes {
 	}
 	initRoutes() {
 		this.router.post("/callback", this.distributedUptimeController.resultsCallback);
+		this.router.get(
+			"/monitors/:teamId",
+			this.distributedUptimeController.getDistributedUptimeMonitors
+		);
+		this.router.get(
+			"/monitors/details/:monitorId",
+			this.distributedUptimeController.getDistributedUptimeMonitorDetails
+		);
 	}
 
 	getRouter() {
