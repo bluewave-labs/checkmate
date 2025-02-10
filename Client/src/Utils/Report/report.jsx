e187fb170cbe5d4616316baa6df50fe28a2cbb7f
Darshan Jain <darshan.j098@gmail.com>
2025-02-10 23:07:00 +0530

    moved report Util function to new file

--- Client/src/Utils/Report/report.jsx
+++ Client/src/Utils/Report/report.jsx
@@ -1,77 +1,8 @@
 import "./reportStyles.css";
 import { formatDurationRounded, formatDateWithWeekday } from "../timeUtils";
+import { calculateDowntimeCount, calculatePingStats, calculateActiveRanges} from "./reportUtils";
 
-const calculateDowntimeCount = (checks) => {
-	if (!checks || checks.length < 2) return 0;
 
-	let downtimeCount = 0;
-	for (let i = 0; i < checks.length - 1; i++) {
-		const currentCheck = checks[i];
-		const nextCheck = checks[i + 1];
-
-		if (currentCheck.status === true && nextCheck.status === false) {
-			downtimeCount++;
-		}
-	}
-	return downtimeCount;
-};
-
-const calculatePingStats = (checks) => {
-	if (!checks || checks.length === 0) return { min: 0, max: 0 };
-
-	let min = Number.MAX_SAFE_INTEGER;
-	let max = 0;
-
-	checks.forEach((check) => {
-		if (check.originalResponseTime) {
-			min = Math.min(min, check.originalResponseTime);
-			max = Math.max(max, check.originalResponseTime);
-		}
-	});
-
-	return {
-		min: min === Number.MAX_SAFE_INTEGER ? 0 : min,
-		max: max,
-	};
-};
-
-const calculateActiveRanges = (checks) => {
-	if (!checks || checks.length < 2) return { min: 0, max: 0 };
-
-	const durations = [];
-	let startTime = null;
-
-	for (let i = checks.length - 1; i >= 0; i--) {
-		const check = checks[i];
-
-		if (check.status && !startTime) {
-			startTime = new Date(check.createdAt);
-		}
-
-		if (!check.status && startTime) {
-			const endTime = new Date(check.createdAt);
-			const duration = endTime - startTime;
-			if (duration > 0) {
-				durations.push(duration);
-			}
-			startTime = null;
-		}
-	}
-
-	// Handle case where site is currently up
-	if (startTime && checks[0].status) {
-		const now = new Date();
-		const duration = now - startTime;
-		durations.push(duration);
-	}
-
-	return durations.length
-		? {
-				min: Math.min(...durations),
-				max: Math.max(...durations),
-			}
-		: { min: 0, max: 0 };
-};
 
 export const ProductReport = ({ monitorData, certificateExpiry }) => {
 	const pingStats = calculatePingStats(monitorData.checks);
