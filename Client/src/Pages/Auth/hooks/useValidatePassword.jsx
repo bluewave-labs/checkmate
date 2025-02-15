2e3279388660bb3e9fb11a9e55dbded727bbdbee
Alex Holliday <ajhollid@gmail.com>
2025-02-14 16:38:01 -0800

    change return type to array for flexible destructuring

--- Client/src/Pages/Auth/hooks/useValidatePassword.jsx
+++ Client/src/Pages/Auth/hooks/useValidatePassword.jsx
@@ -65,6 +65,6 @@ function useValidatePassword() {
 		[form, errors]
 	);
 
-	return { handleChange, feedbacks, form, errors };
+	return [handleChange, feedbacks, form, errors];
 }
 export { useValidatePassword };
